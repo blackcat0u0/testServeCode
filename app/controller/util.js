c10b844ae38d5e9ee86ace6163ec8f8a0f85d296
caokun <caokun@lcom>
2020-12-04 17:19:23 +0800

    📦 验证码接口

--- app/controller/util.js
+++ app/controller/util.js
@@ -5,19 +5,19 @@ const Controller = require('egg').Controller
 class UtilController extends Controller {
   async captcha() {
     const captcha = svgCaptcha.create({
-      size:4,
-      fontSize:50,
-      width:100,
-      height:40,
-      noise:3
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      noise: 3,
     })
     console.log(
-      "captcha=>"+captcha.text
-    );
+      'captcha=>' + captcha.text
+    )
     this.ctx.session.captcha = captcha.text
     this.ctx.response.type = 'image/svg+xml'
     this.ctx.body = captcha.data
-    
+
   }
 }
 
